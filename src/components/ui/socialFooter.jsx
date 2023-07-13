be77653fdd4f4cabb4ac1ce784600ececac339cc
productamrendra <info.makeajodi@gmail.com>
2023-07-13 23:38:52 +0530

    dasd2qweqe

--- src/components/ui/socialFooter.jsx
+++ src/components/ui/socialFooter.jsx
@@ -7,7 +7,7 @@ import {
 
 export default function socialFooter() {
   return (
-    <div className="p-4 border-t-2 border-orange-500 text-white flex justify-center gap-4 bg-rose-600">
+    <div className="p-4 border-t-4 border-orange-400 text-white flex justify-center gap-6 bg-rose-600">
     <SlSocialInstagram />
     <SlSocialTwitter />
     <SlSocialYoutube />
