d874125a2a22a38a65b84a44797f834ea243b0fe
productamrendra <info.makeajodi@gmail.com>
2023-10-19 13:21:55 +0530

    logo changed

--- src/components/ui/navbar.jsx
+++ src/components/ui/navbar.jsx
@@ -1,6 +1,6 @@
 import { memo } from "react";
 import { Link } from "react-router-dom";
-import DropList from "../molecules/menu";
+// import DropList from "../molecules/menu";
 
 function navbar() {
   return (
